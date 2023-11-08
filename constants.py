b8264b50714fef5ab9e0135256b113d157ffb834
Sven den Hartog <denhartog.sven@gmail.com>
2023-11-08 17:58:44 +0100

    Changed action models

---
+++ constants.py
@@ -0,0 +1,195 @@
+from enum import Enum
+from typing import Literal
+
+
+NumberOrX = int | Literal["X"] | Literal["-X"] | Literal["that"]
+
+
+class GrammarEnum(Enum):
+    @classmethod
+    def to_grammar(cls):
+        notnones = [c for c in cls if c != ""]
+        return " | ".join([f'( /[{v.value[0].lower()}{v.value[0].upper()}]/ "{v.value[1:]}")' for v in notnones])
+    
+    @classmethod
+    def to_int(cls, value):
+        vals = list(cls.__members__.values())
+        return vals.index(value)
+
+
+class ColorEnum(str, GrammarEnum):
+    none = ""
+    colorless = "U"
+    yellow = "Y"
+    red = "R"
+    green = "G"
+    blue = "B"
+    multicolored = "M"
+    monocolored = "O"
+
+
+class Activation(str, GrammarEnum):
+    activate = "Q"
+    deactivate = "T"
+
+
+class TypeEnum(str, GrammarEnum):
+    none = ""
+    unit = "unit"
+    item = "item"
+    spell = "spell"
+    token = "token"
+
+
+class KeywordEnum(str, GrammarEnum):
+    none = ""
+    flying = "flying"
+    siege = "siege"
+    poison = "poison"
+
+
+class NumbericalEnum(str, GrammarEnum):
+    damage = "damage"
+    health = "health"
+    level = "level"
+
+
+class ObjectActionEnum(str, GrammarEnum):
+    attack = "attack"
+    block = "block"
+    attackorblock = "attack or block"
+    beblocked = "be blocked"
+    becountered = "be countered"
+    activateability = "activate abilities"
+
+
+class ConditonEnum(str, GrammarEnum):
+    playedwhen = "played when"
+    yourturn = "your turn"
+    notyourturn = "not your turn"
+    compare = "number compare"
+    playercond = "player condition"
+    objectcond = "object condition"
+    thisturn = "this turn"
+
+
+class Reference(str, GrammarEnum):
+    none = ""
+    self = "~"
+    it = "it"
+    they = "they"
+    rest = "the rest"
+    sac = "the sacrificed"
+    any = "any of"
+    this = "this"
+    that = "that"
+    the = "the"
+    another = "another"
+    chosen = "the chosen"
+    each = "each"
+    all = "all"
+    target = "target"
+    atleast = "atleast"
+    exactly = "exactly"
+    ormore = "or more"
+    fewerthan = "fewer than"
+    anynumberof = "any number of"
+    upto = "up to"
+    oneof = "one of"
+    your = "your"
+    their = "their"
+    itself = "itself"
+    anytarget = "any target"
+
+
+Countables = [Reference.exactly, Reference.atleast, Reference.ormore, Reference.fewerthan, Reference.upto, Reference.oneof, Reference.anynumberof]
+
+
+class PrefixEnum(str, GrammarEnum):
+    none = ""
+    activated = "activated"
+    attacking = "attacking"
+    blocking = "blocking"
+    attackingorblocking = "attacking or blocking"
+
+
+class SuffixEnum(str, GrammarEnum):
+    none = ""
+    control = "control"
+    nocontrol = "no control"
+    own = "own"
+    noown = "no own"
+    inzone = "in zone"
+    youplay = "you play"
+    targets = "targets"
+    targetsonly = "targets only"
+    couldtarget = "could target"
+    activatedthisway = "activated this way"
+    deactivatedthisway = "deactivated this way"
+    chosentype = "chosen type"
+    amongthem = "among them"
+
+
+class ZoneEnum(str, GrammarEnum):
+    none = ""
+    deck = "deck"
+    pile = "pile"
+    hand = "hand"
+    board = "board"
+    stack = "stack"
+    it = "it"
+
+
+class OperatorEnum(str, GrammarEnum):
+    AND = "and"
+    OR = "or"
+    XOR = "xor"
+    OPTIONAL = "optional"
+
+
+class PlaceEnum(str, GrammarEnum):
+    none = ""
+    top = "top"
+    bottom = "bottom"
+
+
+class OrderEnum(str, GrammarEnum):
+    ordered = "ordered"
+    random = "random"
+
+
+class PlayerEnum(str, GrammarEnum):
+    none = ""
+    opponent = "opponent"
+    player = "player"
+    you = "you"
+    they = "they"
+    owner = "owner"
+    controller = "controller"
+    defending = "defending"
+    attacking = "attacking"
+
+
+class TriggerEnum(str, GrammarEnum):
+    whenplay = "when play"
+    whengainlife = "when gain life"
+    whenloselife = "when lose life"
+    whendamaged = "when damaged"
+    endofturn = "end of turn"
+    beginningofphase = "beginning of phase"
+
+
+class PhaseEnum(str, GrammarEnum):
+    turn = "turn"
+    activation = "activation"
+    draw = "draw step"
+    play = "play phase"
+    fight = "fight phase"
+    cleanup = "cleanup"
+
+
+class TurnQualifierEnum(str, GrammarEnum):
+    each = "each"
+    this = "this"
+    that = "that"
+    the = "the"
--- models.py
+++ models.py
@@ -1,11 +1,9 @@
-from enum import Enum
 from typing import Literal, Any
 from pydantic import BaseModel
-
 from godot_parser import GDResource, GDSection, GDSectionHeader, GDExtResourceSection, GDSubResourceSection, ExtResource, SubResource
 
+from constants import *
 
-NumberOrX = int | Literal["X"] | Literal["-X"] | Literal["that"]
 
 def encode_numberorx(num: NumberOrX):
     if num == "X":
@@ -15,195 +13,6 @@ def encode_numberorx(num: NumberOrX):
     elif num == "that":
         return 8888888888888888
     return int(num)
-
-
-class GrammarEnum(Enum):
-    @classmethod
-    def to_grammar(cls):
-        notnones = [c for c in cls if c != ""]
-        return " | ".join([f'( /[{v.value[0].lower()}{v.value[0].upper()}]/ "{v.value[1:]}")' for v in notnones])
-    
-    @classmethod
-    def to_int(cls, value):
-        vals = list(cls.__members__.values())
-        return vals.index(value)
-
-
-class ColorEnum(str, GrammarEnum):
-    none = ""
-    colorless = "U"
-    yellow = "Y"
-    red = "R"
-    green = "G"
-    blue = "B"
-    multicolored = "M"
-    monocolored = "O"
-
-
-class Activation(str, GrammarEnum):
-    activate = "Q"
-    deactivate = "T"
-
-
-class TypeEnum(str, GrammarEnum):
-    none = ""
-    unit = "unit"
-    spell = "spell"
-    token = "token"
-
-
-class KeywordEnum(str, GrammarEnum):
-    none = ""
-    flying = "flying"
-    siege = "siege"
-    poison = "poison"
-
-
-class NumbericalEnum(str, GrammarEnum):
-    damage = "damage"
-    health = "health"
-    level = "level"
-
-
-class ObjectActionEnum(str, GrammarEnum):
-    attack = "attack"
-    block = "block"
-    attackorblock = "attack or block"
-    beblocked = "be blocked"
-    becountered = "be countered"
-    activateability = "activate abilities"
-
-
-class ConditonEnum(str, GrammarEnum):
-    playedwhen = "played when"
-    yourturn = "your turn"
-    notyourturn = "not your turn"
-    compare = "number compare"
-    playercond = "player condition"
-    objectcond = "object condition"
-    thisturn = "this turn"
-
-
-class Reference(str, GrammarEnum):
-    none = ""
-    self = "~"
-    it = "it"
-    they = "they"
-    rest = "the rest"
-    sac = "the sacrificed"
-    any = "any of"
-    this = "this"
-    that = "that"
-    the = "the"
-    another = "another"
-    chosen = "the chosen"
-    each = "each"
-    all = "all"
-    target = "target"
-    atleast = "atleast"
-    exactly = "exactly"
-    ormore = "or more"
-    fewerthan = "fewer than"
-    anynumberof = "any number of"
-    upto = "up to"
-    oneof = "one of"
-    your = "your"
-    their = "their"
-    itself = "itself"
-    anytarget = "any target"
-
-
-Countables = [Reference.exactly, Reference.atleast, Reference.ormore, Reference.fewerthan, Reference.upto, Reference.oneof, Reference.anynumberof]
-
-
-class PrefixEnum(str, GrammarEnum):
-    none = ""
-    activated = "activated"
-    attacking = "attacking"
-    blocking = "blocking"
-    attackingorblocking = "attacking or blocking"
-
-
-class SuffixEnum(str, GrammarEnum):
-    none = ""
-    control = "control"
-    nocontrol = "no control"
-    own = "own"
-    noown = "no own"
-    inzone = "in zone"
-    youplay = "you play"
-    targets = "targets"
-    targetsonly = "targets only"
-    couldtarget = "could target"
-    activatedthisway = "activated this way"
-    deactivatedthisway = "deactivated this way"
-    chosentype = "chosen type"
-    amongthem = "among them"
-
-
-class ZoneEnum(str, GrammarEnum):
-    none = ""
-    deck = "deck"
-    pile = "pile"
-    hand = "hand"
-    board = "board"
-    stack = "stack"
-    it = "it"
-
-
-class OperatorEnum(str, GrammarEnum):
-    AND = "and"
-    OR = "or"
-    XOR = "xor"
-    OPTIONAL = "optional"
-
-
-class PlaceEnum(str, GrammarEnum):
-    none = ""
-    top = "top"
-    bottom = "bottom"
-
-
-class OrderEnum(str, GrammarEnum):
-    ordered = "ordered"
-    random = "random"
-
-
-class PlayerEnum(str, GrammarEnum):
-    none = ""
-    opponent = "opponent"
-    player = "player"
-    you = "you"
-    they = "they"
-    owner = "owner"
-    controller = "controller"
-    defending = "defending"
-    attacking = "attacking"
-
-
-class TriggerEnum(str, GrammarEnum):
-    whenplay = "when play"
-    whengainlife = "when gain life"
-    whenloselife = "when lose life"
-    whendamaged = "when damaged"
-    endofturn = "end of turn"
-    beginningofphase = "beginning of phase"
-
-
-class PhaseEnum(str, GrammarEnum):
-    turn = "turn"
-    activation = "activation"
-    draw = "draw step"
-    play = "play phase"
-    fight = "fight phase"
-    cleanup = "cleanup"
-
-
-class TurnQualifierEnum(str, GrammarEnum):
-    each = "each"
-    this = "this"
-    that = "that"
-    the = "the"
 
 
 class Stats(BaseModel):
@@ -721,10 +530,24 @@ class GetAbility(BaseEffect):
     abilities: list[AquiredAbilities | ModAbility | Literal["this"]] = []
     until: Condition | None = None
 
+    def to_godot(self, resource: GDResource):
+        ability = resource.add_sub_resource("Resource")
+        ability["script"] = resource.add_ext_resource("res://effects/GetAbility.gd", "Script").reference
+        ability["abilities"] = [ability_to_godot(resource, a) for a in self.abilities]
+        ability["until"] = self.until.to_godot(resource) if self.until else None
+        return ability.reference
+
 
 class CantAbility(BaseEffect):
     actions: list[ObjectActionEnum] = []
     until: Condition | None = None
+
+    def to_godot(self, resource: GDResource):
+        ability = resource.add_sub_resource("Resource")
+        ability["script"] = resource.add_ext_resource("res://effects/CantAbility.gd", "Script").reference
+        ability["actions"] = [ObjectActionEnum.to_int(a) for a in self.actions]
+        ability["until"] = self.until.to_godot(resource) if self.until else None
+        return ability.reference
 
 
 class NoActivationAbility(BaseEffect):
